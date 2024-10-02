0c14b6d10f437817f2520009da3fb5c36315b135
Patrick Teivas <29047825+patrickteivas@users.noreply.github.com>
2024-10-02 10:09:44 +0300

    Initialize project

---
+++ js/main.js
@@ -0,0 +1,29 @@
+;(function () {
+    'use strict';
+
+    // // Document on load.
+    // $(function(){
+    //     fullHeight();
+    //     sliderMain();
+    //     sliderSayings();
+    //     offcanvasMenu();
+    //     mainMenuSticky();
+    //     parallax();
+    //     burgerMenu();
+    //     scrolledWindow();
+    //     clickMenu();
+    //     navigationSection();
+    //     goToTop();
+    //
+    //     // Animations
+    //     homeAnimate();
+    //     aboutAnimate();
+    //     sayingsAnimate();
+    //     featureAnimate();
+    //     typeAnimate();
+    //     foodMenusAnimate();
+    //     eventsAnimate();
+    //     reservationAnimate();
+    //     footerAnimate();
+    // });
+}());
